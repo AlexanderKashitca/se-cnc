4126a12f7269d9deaebe144c2b3a91eeaa0a3bca
AlexanderKashitca <se.cnc.comp@gmail.com>
2019-07-17 17:41:19 +0300

    restruct repozitory

---
+++ src/kinematics/kinematics5AxisTableAC.h
@@ -0,0 +1,31 @@
+///-----------------------------------------------------------------------------
+/// Kinematics5AxisTableAC.h: interface for the CKinematics5AxisTableAC class.
+///-----------------------------------------------------------------------------
+#ifndef KINEMATICS_5_AXIS_TABLE_AC_H
+#define KINEMATICS_5_AXIS_TABLE_AC_H
+///-----------------------------------------------------------------------------
+#include <math.h>
+///-----------------------------------------------------------------------------
+///#include "kinematics.h"
+///-----------------------------------------------------------------------------
+class Kinematics5AxisTableACClass : public KinematicsClass
+{
+public:
+    Kinematics5AxisTableACClass();
+    virtual ~Kinematics5AxisTableACClass();
+	virtual int TransformCADtoActuators(double x, double y, double z, double a, double b, double c, double *Acts, bool NoGeo = false);
+	virtual int TransformActuatorstoCAD(double *Acts, double *x, double *y, double *z, double *a, double *b, double *c, bool NoGeo = false);
+	int InvertTransformCADtoActuators(double *Acts, double *xr, double *yr, double *zr, double *ar, double *br, double *cr, bool NoGeo = false);
+
+	void Rotate3(double xc,double yc,double zc,double x,double y,double z,double a,double b,double c,
+								double *xp,double *yp,double *zp);
+
+        double CTableAZeroZZero;
+        double ASaddleYCenterpoint;
+        double ASaddleZCenterpoint;
+        double CTableAZeroXCenterpoint;
+        double CTableAZeroYCenterpoint;
+};
+///-----------------------------------------------------------------------------
+#endif /// KINEMATICS_5_AXIS_TABLE_AC_H
+///-----------------------------------------------------------------------------
---
+++ src/kinematics/kinematicsGeppetto.h
@@ -0,0 +1,45 @@
+///-----------------------------------------------------------------------------
+/// KinematicsGeppetto.h: interface for the CKinematicsGeppetto class.
+///-----------------------------------------------------------------------------
+#ifndef KINEMATICS_GEPPETTO_H
+#define KINEMATICS_GEPPETTO_H
+///-----------------------------------------------------------------------------
+#include <math.h>
+///-----------------------------------------------------------------------------
+//#include "kinematics.h"
+///-----------------------------------------------------------------------------
+class KinematicsGeppettoClass : public KinematicsClass
+{
+public:
+	CPT3D Act0Center;
+	CPT3D Act1Center;
+	CPT3D Act2Center;
+	CPT3D Act3Center;
+	CPT3D Act4Center;
+	CPT3D Act5Center;
+	double Act0Off;
+	double Act1Off;
+	double Act2Off;
+	double Act3Off;
+	double Act4Off;
+	double Act5Off;
+    KinematicsGeppettoClass();
+    virtual ~KinematicsGeppettoClass();
+	virtual int TransformCADtoActuators(double x, double y, double z, double a, double b, double c, double *Acts, bool NoGeo = false);
+	virtual int TransformActuatorstoCAD(double *Acts, double *x, double *y, double *z, double *a, double *b, double *c, bool NoGeo = false);
+	int InvertTransformCADtoActuators(double *Acts, double *xr, double *yr, double *zr, double *ar, double *br, double *cr, bool NoGeo = false);
+	virtual int ReadGeoTable(const char *name);
+	int GeoCorrect(double x, double y, double z, double a, double b, double c,
+		double *rx, double *ry, double *rz, double *ra, double *rb, double *rc);
+
+	void Rotate(double xc,double yc,double zc,double z,double a,double b,double *xp,double *yp,double *zp);
+	void Rotate3(double xc,double yc,double zc,double x,double y,double z,double a,double b,double c,
+								double *xp,double *yp,double *zp);
+
+	virtual int GetSoftLimits(double *xm, double *xp, double *ym, double *yp, double *zm, double *zp,
+		double *am, double *ap, double *bm, double *bp, double *cm, double *cp);
+
+};
+///-----------------------------------------------------------------------------
+#endif /// KINEMATICS_GEPPETTO_H
+///-----------------------------------------------------------------------------
